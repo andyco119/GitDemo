2dd555fcbdc81633c7de496cd9b921fe3dd56bc2
Kozłowski Andrzej <kozlowa@omnis.netia.org>
2021-06-22 13:53:12 +0200

    first commit

---
+++ Lab4_Zad7.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+char * napis = "Witajcie moi mili ...";
+int main(){
+  unsigned int index =0;
+  for (index; ; index++){
+    printf("%c",napis[index]);
+  }
+  return 0;
+}
---
+++ Lab4_Zad7cos.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+char * napis = "Witajcie moi mili ...";
+int main(){
+  unsigned int index =0;
+  for (index; ; index++){
+    printf("%c",napis[index]);
+  }
+  //sdfsfsfs
+  ///sfsfsff
+  
+  
+  return 0;
+}
