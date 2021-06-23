16b729cbe41e747003087426b0caceae9be1b063
Kozłowski Andrzej <kozlowa@omnis.netia.org>
2021-06-23 09:48:52 +0200

    master nowy plik

---
+++ Lab4_Zad7cos2.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+char * napis = "Witajcie moi mili ...";
+int main(){
+  unsigned int index =0;
+  for (index; ; index++){
+    printf("%c",napis[index]);
+  }
+  printf("dupa11");
+  
+  
+  return 0;
+}
